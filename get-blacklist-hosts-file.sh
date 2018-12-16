8a6969ddc922d880adca3758ea1083b4d8947b28
Dan Pasanen <dan.pasanen@gmail.com>
2018-12-16 09:51:16 -0600

    hosts: allow www.googleadservices.com
    this is annoying not to have in google results

--- get-blacklist-hosts-file.sh
+++ get-blacklist-hosts-file.sh
@@ -4,3 +4,5 @@ mkdir -p vendor/extra/rootdir/etc/
 rm -f vendor/extra/rootdir/etc/hosts
 
 wget --quiet https://github.com/StevenBlack/hosts/raw/master/hosts -O vendor/extra/rootdir/etc/hosts
+
+sed -i 's/0.0.0.0 www.googleadservices.com/#0.0.0.0 www.googleadservices.com/' vendor/extra/rootdir/etc/hosts
