168e3e2a409f108a928b1a788e07a375a8d58708
Dan Pasanen <dan.pasanen@gmail.com>
2019-02-07 12:51:15 -0600

    whitelist a few m$ pages
    * https://www.trueachievements.com/forum/viewthread.aspx?tid=998988

--- get-blacklist-hosts-file.sh
+++ get-blacklist-hosts-file.sh
@@ -5,4 +5,34 @@ rm -f vendor/extra/rootdir/etc/hosts
 
 wget --quiet https://github.com/StevenBlack/hosts/raw/master/hosts -O vendor/extra/rootdir/etc/hosts
 
-sed -i 's/0.0.0.0 www.googleadservices.com/#0.0.0.0 www.googleadservices.com/' vendor/extra/rootdir/etc/hosts
+whitelists=( \
+  activity.windows.com \
+  attestation.xboxlive.com \
+  cert.mgt.xboxlive.com \
+  continuum.dds.microsoft.com \
+  ctldl.windowsupdate.com \
+  def-vef.xboxlive.com \
+  device.auth.xboxlive.com \
+  eds.xboxlive.com \
+  help.ui.xboxlive.com \
+  licensing.xboxlive.com \
+  mobile.pipe.aria.microsoft.com \
+  notify.xboxlive.com \
+  settings-win.data.microsoft.com \
+  title.auth.xboxlive.com \
+  title.mgt.xboxlive.com \
+  v10.vortex-win.data.microsoft.com \
+  v20.vortex-win.data.microsoft.com \
+  www.googleadservices.com \
+  www.msftncsi.com \
+  www.xboxlive.com \
+  xboxexperiencesprod.experimentation.xboxlive.com \
+  xbox.ipv6.microsoft.com \
+  xflight.xboxlive.com \
+  xkms.xbolive.com \
+  xsts.auth.xboxlive.com \
+)
+
+for site in ${whitelists[*]}; do
+  sed -i 's/0.0.0.0 '$site'/#0.0.0.0 '$site'/' vendor/extra/rootdir/etc/hosts
+done
