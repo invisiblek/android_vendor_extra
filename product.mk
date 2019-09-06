a44c3cc4d3e94b0f79fafd5deb2477a58f77561f
Dan Pasanen <dan.pasanen@gmail.com>
2019-09-06 08:36:21 -0500

    extra: move to luca's gapps repo

--- product.mk
+++ product.mk
@@ -3,14 +3,7 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 $(call inherit-product-if-exists, vendor/invisiblek/product.mk)
 
 ifneq ($(filter lineage_marlin lineage_mata lineage_nash lineage_sailfish,$(TARGET_PRODUCT)),)
-GAPPS_VARIANT := nano
-$(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
-
-PRODUCT_COPY_FILES += \
-  vendor/opengapps/sources/arm/lib/28/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
-  vendor/opengapps/sources/arm/lib/28/libsketchology_native.so:system/lib/libsketchology_native.so \
-  vendor/opengapps/sources/arm64/lib64/28/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so \
-  vendor/opengapps/sources/arm64/lib64/28/libsketchology_native.so:system/lib64/libsketchology_native.so
+$(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
 endif
 
 $(shell sh vendor/extra/get-blacklist-hosts-file.sh)
